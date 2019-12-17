2fd310ce8571c42d242034a65cb7c23f4ea65612
Tim Langer <timlanger@Tims-MacBook-Pro.local>
2019-12-16 21:27:17 -0500

    Made slight changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('This is working');
+const contentElement = document.querySelector('#content')
 
 const products = fetch('https://acme-users-api-rev.herokuapp.com/api/products');
 const companies = fetch(
@@ -7,10 +7,22 @@ const companies = fetch(
 const offerings = fetch(
   'https://acme-users-api-rev.herokuapp.com/api/offerings'
 );
+
+function renderProductCards(arr){
+  arr.forEach(function (el){
+    const newElement = document.createElement('div')
+    newElement.innerHTML =
+   `<h1>${el.name}</h1>
+   <h4>${el.description}</h4>
+   <h4>$${el.suggestedPrice}.00</h4>
+  `
+  contentElement.appendChild(newElement)
+    newElement.classList.add('product-card')
+  })
+ }
+
 async function getData() {
   const responses = await Promise.all([products, companies, offerings]);
-  console.log(responses);
-
   const productsJSON = await responses[0].json();
   const companiesJSON = await responses[1].json();
   const offeringsJSON = await responses[2].json();
@@ -18,8 +30,99 @@ async function getData() {
   console.log('products', productsJSON);
   console.log('companies', companiesJSON);
   console.log('offerings', offeringsJSON);
-}
-getData();
+   renderProductCards(productsJSON)
+
+  //  getCompanyIds(companiesJSON)
+  }
+
+  getData()
+
+  // function getCompanyIds(arr){
+  //   arr.forEach(el => {
+  //     console.log(el.name)
+  //   })
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  function addOfferingList(){
+  //   firstProductOfferings.forEach( el => {
+  //     const newOfferingPriceList = document.createElement('ul')
+  //     newOfferingPriceList.innerHTML =
+  //     `
+  //     <li>Offered at: ${companiesJSON[1].name} ${el.price}</li>
+  //     `
+  //     productCards.appendChild(newOfferingPriceList)
+  //   })
+  //  }
+  //  addOfferingList()
+  //  const data = []
+  //  return data.push[productsJSON, companiesJSON, offeringsJSON]
+  //  getOfferingPrice()
+
+
+//  const firstProductOfferings =[]
+//  function getOfferingPrice(){
+//    offeringsJSON.forEach(offering => {
+//      if (offering.productId === productsJSON[0].id){
+//        firstProductOfferings.push(offering)
+//      }
+//    })
+//   return firstProductOfferings
+//  }
+
+
+
+
+
+
+
+
+
+// productsJSON.forEach(el => {
+//   const productDescriptionElement = document.createElement('div')
+//   productDescriptionElement.innerHTML =
+//   `
+//   <p>${el.description}</p>
+//   `
+//   productNameElement.appendChild(productDescriptionElement)
+// })
+
+
+  // newProductElement.innerText = `${productsJSON[0].name}`
+// }
+;
 
 // async function buildOne() {}
 // async function getProductData() {
